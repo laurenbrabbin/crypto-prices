66c32818d8c346b8c3743e2301c5ede49875356a
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-16 09:14:33 +0000

    test 9 passing

--- 01_typescript/src/exercises.spec.ts
+++ 01_typescript/src/exercises.spec.ts
@@ -235,20 +235,28 @@ describe("What is typescript?", () => {
     Uncomment the following tests and make them pass.
   */
 
-  // it("has interfaces 1", () => {
-  //   interface PersonModel {
-  //     setName(name: string): void;
-  //     getName(): string;
-  //   }
-  //
-  //   class Person implements PersonModel {
-  //
-  //   }
-  //
-  //   let person = new Person();
-  //   person.setName("Kay");
-  //   expect(person.getName()).toEqual("Kay");
-  // });
+  it("has interfaces 1", () => {
+    interface PersonModel {
+      setName(name: string): void;
+      getName(): string;
+    }
+  
+    class Person implements PersonModel {
+      name!: string
+
+      setName(name: string):void {
+         this.name = name
+      }
+
+      getName():string {
+        return this.name
+     }
+    }
+  
+    let person = new Person();
+    person.setName("Kay");
+    expect(person.getName()).toEqual("Kay");
+  });
 
   // it("has interfaces 2", () => {
   //   // This is a little trickier.
