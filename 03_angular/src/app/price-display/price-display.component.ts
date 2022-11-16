4064459043ad512cc7582038026c5e2007bb0656
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-16 10:12:27 +0000

    price refreshing every 10 seconds

--- 03_angular/src/app/price-display/price-display.component.ts
+++ 03_angular/src/app/price-display/price-display.component.ts
@@ -1,21 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PriceService } from '../price.service';
+import {Subscription, timer} from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'price-display',
   templateUrl: './price-display.component.html',
   styleUrls: ['./price-display.component.css']
 })
+
 export class PriceDisplayComponent implements OnInit {
   @Input() from!: string;
   @Input() to!: string;
   price!: number;
+  timerSubscription!: Subscription;
 
   constructor(private priceService: PriceService) {}
 
-  ngOnInit(): void {
-    this.loadPriceData();
-  }
+  ngOnInit(): void { 
+    this.timerSubscription = timer(0, 10000).pipe( 
+    map(() => { 
+      this.loadPriceData();
+    }) 
+  ).subscribe(); 
+} 
 
   loadPriceData() {
     this.priceService.getPrice(this.from, this.to).subscribe(price => {
